2cddb069ba13a740dd8f467fbb1d099b1118018a
DaFunEDeveloper <62800011+DaFunEDeveloper@users.noreply.github.com>
2020-03-28 13:33:13 -0400

    Create math.js

---
+++ math.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>The Jacob Homemade Multiplier Machine</title>
+</head>
+<body>
+
+</body>
+</html>
+var noOne = prompt("The First Number:", "")
+var noTwo = prompt("The Second Number:","")
+alert(noOne * noTwo)
